1de1095b1c919ab6e2bd79cc502350946418729a
arun <arunsolanki301@gmail.com>
2023-07-21 12:29:39 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { FiltercomponentComponent } from './filtercomponent/filtercomponent.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    FiltercomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/container/container.component.ts
@@ -0,0 +1,95 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector : 'app-container',
+  templateUrl : './container.component.html',
+  styleUrls : ['./container.component.css',]
+})
+
+export class ContainerComponent{
+
+  inputValue : string ="";
+  err : string = "";
+  isEdit: boolean = false;
+  todoArray : {title :string,id:number, checked :boolean}[] = [];
+  selectedFilterOption : string ='All'
+  editId : number = NaN;
+  ngOnInit(){
+    const data = localStorage.getItem('todoArray')
+    this.todoArray = data == null ? [] : JSON.parse(data)
+  }
+
+  onChange(e : Event){
+    this.inputValue = (<HTMLInputElement>e.target).value
+  }
+
+  addTodo(){
+    if(!this.inputValue.length){
+      this.err = "input should contain a task"
+    }else{
+      this.todoArray.push({
+        title : this.inputValue,
+        id : this.todoArray.length + 1,
+        checked : false
+      })
+      localStorage.setItem('todoArray' , JSON.stringify(this.todoArray))
+      this.inputValue = ""
+    }
+  }
+
+  onDelete(id :number){
+    const arr = this.todoArray.filter((item : any)=>{
+      return item.id !== id
+    })
+    localStorage.setItem('todoArray' , JSON.stringify(arr))
+    this.todoArray=arr
+  }
+  onEdit(id : number){
+    this.isEdit = true;
+    this.editId = id;
+    for(let item of this.todoArray){
+      if(item.id === id){
+        this.inputValue = item.title;
+        break
+      }
+     }
+   
+  }
+
+  checkValue(e : Event , id : number){
+    const data = this.todoArray.map((item : {title :string,id:number, checked :boolean})=>{
+      if(id == item.id){
+        return{...item , checked : (<HTMLInputElement>e.target).checked}
+      }else{
+        return item
+      }
+    })
+
+    localStorage.setItem('todoArray' , JSON.stringify(data))
+    this.todoArray = data;
+  }
+
+  filterItems(e : string){
+    this.selectedFilterOption = e
+  }
+  
+
+  updateTodo(){
+    const data = this.todoArray.map(item=>{
+      if(item.id == this.editId){
+        item.title = this.inputValue
+        item.checked = false
+        return item
+      }else{
+        return item
+      }
+    })
+    localStorage.setItem('todoArray' , JSON.stringify(data))
+    this.todoArray = data;
+    this.isEdit = false;
+    this.inputValue = ""
+    this.editId = NaN;
+  }
+
+}
